3c498abe4041922cab81c8e6c0661f40a7a70d02
dyovu <131934396+dyovu@users.noreply.github.com>
2026-03-12 19:32:38 +0900

    elf形式のカーネルを呼ぶように変更

--- day05/bootloader-05/src/main.rs
+++ day05/bootloader-05/src/main.rs
@@ -6,13 +6,14 @@ use alloc::format;
 use alloc::vec;
 use alloc::vec::Vec;
 use core::arch::asm;
+use core::ptr::{copy_nonoverlapping, write_bytes};
 use log::info;
 
-use uefi::boot::{exit_boot_services, memory_map, image_handle, MemoryType};
+use uefi::boot::{image_handle, exit_boot_services, memory_map, MemoryType, allocate_pages};
 use uefi::mem::memory_map::{MemoryMap, MemoryMapOwned};
 use uefi::prelude::*;
 use uefi::proto::console::gop::GraphicsOutput;
-use uefi::proto::media::file::{self, File, FileAttribute, FileMode};
+use uefi::proto::media::file::{self, File, FileAttribute, FileMode, FileInfo};
 
 // ================================================================
 // 構造体定義
@@ -36,6 +37,39 @@ pub struct RawMemoryDescriptor {
     pub virtual_start: u64,
     pub number_of_pages: u64,
     pub attribute: u64,
+}
+
+// ELFファイルを解析するための構造体
+// ファイルヘッダの構造体
+#[repr(C)]
+pub struct Elf64_Ehdr {
+    pub e_ident: [u8; 16],
+    pub e_type: u16,
+    pub e_machine: u16,
+    pub e_version: u32,
+    pub e_entry: u64,
+    pub e_phoff: u64,
+    pub e_shoff: u64,
+    pub e_flags: u32,
+    pub e_ehsize: u16,
+    pub e_phentsize: u16,
+    pub e_phnum: u16,
+    pub e_shentsize: u16,
+    pub e_shnum: u16,
+    pub e_shstrndx: u16,
+}
+
+// プログラムヘッダの構造体
+#[repr(C)]
+pub struct Elf64_Phdr {
+    pub p_type: u32,
+    pub p_flags: u32,
+    pub p_offset: u64,
+    pub p_vaddr: u64,
+    pub p_paddr: u64,
+    pub p_filesz: u64,
+    pub p_memsz: u64,
+    pub p_align: u64,
 }
 
 // ================================================================
@@ -116,7 +150,7 @@ fn get_framebuffer_info() -> Result<FrameBufferInfo, uefi::Error> {
 }
 
 // ================================================================
-// カーネルファイルの読み込み
+// カーネルファイルの読み込み、コピー
 // ================================================================
 
 fn load_kernel(image_handle: uefi::Handle) -> Result<Vec<u8>, uefi::Error> {
@@ -124,20 +158,69 @@ fn load_kernel(image_handle: uefi::Handle) -> Result<Vec<u8>, uefi::Error> {
     let mut root: file::Directory = file_system.open_volume()?;
 
     let mut kernel_file = root
-        .open(cstr16!("kernel.bin"), FileMode::Read, FileAttribute::empty())
+        .open(cstr16!("kernel.elf"), FileMode::Read, FileAttribute::empty())
         .expect("Failed to open kernel file")
         .into_regular_file()
         .expect("Kernel file is not a regular file");
 
+    // Cではsize_ofでファイル名の長さに応じたサイズを動的に決められるが、rustではできない
+    // 一度サイズ0でget_infoをよびだすか、下記のようにある程度大きなbufを用意しておく
     let mut info_buffer = vec![0u8; 1024];
+    // ファイルのメタ情報を読み込む
     let file_info = kernel_file
-        .get_info::<uefi::proto::media::file::FileInfo>(&mut info_buffer)
+        .get_info::<FileInfo>(&mut info_buffer)
         .expect("Failed to get file info");
 
     let mut buffer = vec![0u8; file_info.file_size() as usize];
     kernel_file.read(&mut buffer)?;
 
     Ok(buffer)
+}
+
+// 読み込むカーネルのアドレス範囲を計算する
+fn calc_load_address_range(elf_Header:*const Elf64_Ehdr) -> Result<(u64, u64),uefi::Error >{
+    let ehdr_ref = unsafe { &*elf_Header };
+    let phdr =(elf_Header as usize + ehdr_ref.e_phoff as usize) as *const Elf64_Phdr;
+    // Rustではポインタに対してC言語のように直接 [i] でアクセスできないため、ポインタからスライス（配列）を作る
+    let phdr_slice = unsafe { core::slice::from_raw_parts(phdr, ehdr_ref.e_phnum as usize) };
+
+    let mut first = u64::MAX;
+    let mut last:u64 =  0;
+
+    for i in 0..ehdr_ref.e_phnum as usize{
+        // PT_LOAD の値は通常 1 らしい
+        if phdr_slice[i].p_type != 1 {continue}
+        first = first.min(phdr_slice[i].p_vaddr);
+        last = last.max(phdr_slice[i].p_vaddr + phdr_slice[i].p_memsz);
+    }
+    Ok((first, last))
+}
+
+// 実際にカーネルのロードセクションをメモリの指定の位置にコピーする
+fn copy_load_segments(elf_Header:*const Elf64_Ehdr) {
+    let ehdr_ref = unsafe { &*elf_Header };
+    let phdr =(elf_Header as usize + ehdr_ref.e_phoff as usize) as *const Elf64_Phdr;
+    let phdr_slice = unsafe { core::slice::from_raw_parts(phdr, ehdr_ref.e_phnum as usize) };
+
+    for i in 0..ehdr_ref.e_phnum as usize{
+        if phdr_slice[i].p_type != 1 {continue}
+        let phdr_ref = &phdr_slice[i];
+        let segm_in_file = elf_Header as usize+ phdr_ref.p_offset as usize;
+        unsafe {
+            copy_nonoverlapping(
+                segm_in_file as *const u8, 
+                phdr_ref.p_vaddr as *mut u8, 
+                phdr_ref.p_filesz as usize
+            );
+
+            write_bytes(
+                (phdr_ref.p_vaddr + phdr_ref.p_filesz) as *mut u8,
+                0,
+                (phdr_ref.p_memsz - phdr_ref.p_filesz) as usize
+            );
+        }
+
+    }
 }
 
 // ================================================================
@@ -161,6 +244,7 @@ fn main() -> Status {
         }
     };
 
+    
     // メモリマップの取得
     let mt = MemoryType::LOADER_DATA;
     let memory_map = match memory_map(mt) {
@@ -202,8 +286,16 @@ fn main() -> Status {
     let desc_size = memory_map.meta().desc_size as u64;
     info!("Memory entries: {}, descriptor size: {}", entry_count, desc_size);
 
+
     // カーネルの読み込み
-    let kernel_data = load_kernel(image_handle).expect("Failed to load kernel");
+    let mut kernel_data = load_kernel(image_handle).expect("Failed to load kernel");
+    let elf_Header = unsafe {kernel_data.as_mut_ptr() as *const Elf64_Ehdr};
+    let (kernel_first_addr, kernel_last_addr) = calc_load_address_range(elf_Header).expect("Failed calculate address range");
+
+    let page_num = ((kernel_last_addr - kernel_first_addr + 0xfff) / 0x1000 )as usize;
+    allocate_pages(boot::AllocateType::Address(kernel_first_addr), MemoryType::LOADER_DATA, page_num).expect("Failed to allocate kernel address");
+    copy_load_segments(elf_Header);
+    
     info!("Kernel: {} bytes, magic={:02x}{:02x}{:02x}{:02x}",
         kernel_data.len(),
         kernel_data[0], kernel_data[1], kernel_data[2], kernel_data[3]
@@ -212,12 +304,14 @@ fn main() -> Status {
     // ブートサービス終了
     let _memory_map_final: MemoryMapOwned = unsafe { exit_boot_services(Some(mt)) };
 
-    // カーネルを0x100000にコピーしてジャンプ
-    let kernel_entry = 0x100000 as *mut u8;
+    // 
+    let ehdr_ref = unsafe { &*elf_Header };
+    let entry_point = ehdr_ref.e_entry as usize;
+    type KernelMain = extern "C" fn(info: &FrameBufferInfo, mmap: &[RawMemoryDescriptor; 200]) -> !;
     unsafe {
-        core::ptr::copy_nonoverlapping(kernel_data.as_ptr(), kernel_entry, kernel_data.len() + 12);
+        let kernel_main: KernelMain = core::mem::transmute(entry_point);
 
-        let kernel_main: extern "C" fn() -> ! = core::mem::transmute(kernel_entry);
-        kernel_main();
+        kernel_main(&framebuffer_info, &memory_entries);
     }
+    
 }
