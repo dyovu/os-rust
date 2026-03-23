6e3fa9143ab65f9e921c7d487bc51724e92acde2
dyovu <131934396+dyovu@users.noreply.github.com>
2026-03-23 15:21:48 +0900

    console.rsをPixelWriter構造体に変更

--- day05/kernel-05/src/graphics/console.rs
+++ day05/kernel-05/src/graphics/console.rs
@@ -2,82 +2,93 @@
 // 文字を描画する際の、改行、スクロールのプログラム
 // ================================================================
 use core::ptr::{copy_nonoverlapping, write_bytes};
+use core::fmt;
 
 use crate::graphics::font::draw_char;
-use crate::graphics::framebuffer::set_pixel;
+use crate::graphics::framebuffer::{PixelWriter, PixelColor};
 
 const ROWS: usize = 25;
 const COLUMNS: usize = 80;
 
 #[repr(C)]
-pub struct Console{
+pub struct Console {
+    writer: PixelWriter,
+    fg_color: PixelColor,
+    bg_color: PixelColor,
     buffer: [[char; COLUMNS + 1]; ROWS],
     cursor_row: usize,
     cursor_column: usize,
 }
 
-impl Console{
-    pub fn new() -> Self{
-        Self{
+impl Console {
+    pub fn new(writer: PixelWriter, fg_color: PixelColor, bg_color: PixelColor) -> Self {
+        Self {
+            writer,
+            fg_color,
+            bg_color,
             buffer: [[' '; COLUMNS + 1]; ROWS],
             cursor_row: 0,
             cursor_column: 0,
         }
     }
 
-    pub fn put_string(&mut self, text: &str, fb_buffer: u64, stride: u64, color: u32) {
-        for c in text.chars(){
-            if c == '\n'{
-                self.new_line(fb_buffer, stride, color);
-            }else if self.cursor_column < COLUMNS{
-                draw_char(fb_buffer, stride, 8*self.cursor_column as u64, 16*self.cursor_row as u64, c, color);
+    pub fn put_string(&mut self, text: &str) {
+        for c in text.chars() {
+            if c == '\n' {
+                self.new_line();
+            } else if self.cursor_column < COLUMNS {
+                draw_char(&self.writer, 8 * self.cursor_column as u64, 16 * self.cursor_row as u64, c, self.fg_color);
                 self.buffer[self.cursor_row][self.cursor_column] = c;
                 self.cursor_column += 1;
             }
         }
     }
 
-    fn new_line(&mut self, fb_buffer: u64, stride: u64, color: u32){
+    fn new_line(&mut self) {
         self.cursor_column = 0;
-        if self.cursor_row < ROWS-1{
+        if self.cursor_row < ROWS - 1 {
             self.cursor_row += 1;
-        }else{
+        } else {
             for y in 0..16 * ROWS {
                 for x in 0..8 * COLUMNS {
-                    set_pixel(fb_buffer, x as u64, y as u64, stride, 0x00_00_00_FF);
+                    self.writer.write(x as u64, y as u64, self.bg_color);
                 }
             }
 
             // 配列の要素を指定した分シフトするメソッド
             // 基本的にこれを使えばいいけど、memcopyとmemsetのような形で実装するために
             // 生ポインタで実装する
-            // 
+            //
             // self.buffer.rotate_left(1);
-            // 
+            //
 
-            for i in 0..ROWS -1{
-                let tmp = self.buffer[i+1].as_ptr();
-                let fil = self.buffer[i].as_mut_ptr();
-                unsafe{
+            for i in 0..ROWS - 1 {
+                unsafe {
                     copy_nonoverlapping(
-                        tmp,
-                        fil,
-                        COLUMNS+1,
-                    )
+                        self.buffer[i + 1].as_ptr(),
+                        self.buffer[i].as_mut_ptr(),
+                        COLUMNS + 1,
+                    );
                 }
-
                 for (col, c) in self.buffer[i].iter().enumerate() {
-                    draw_char(fb_buffer, stride, (8 * col) as u64, (16 * i) as u64, *c, color);
+                    draw_char(&self.writer, (8 * col) as u64, (16 * i) as u64, *c, self.fg_color);
                 }
             }
 
-            unsafe{
+            unsafe {
                 write_bytes(
-                    self.buffer[ROWS-1].as_mut_ptr(),
-                    0, 
-                    COLUMNS+1,
+                    self.buffer[ROWS - 1].as_mut_ptr(),
+                    0,
+                    COLUMNS + 1,
                 );
             }
         }
+    }
+}
+
+impl fmt::Write for Console {
+    fn write_str(&mut self, s: &str) -> fmt::Result {
+        self.put_string(s);
+        Ok(())
     }
 }
