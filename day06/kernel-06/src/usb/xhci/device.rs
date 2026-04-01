9c04648f757f1826840f334c782484ffaf6f48ac
dyovu <131934396+dyovu@users.noreply.github.com>
2026-04-01 18:06:10 +0900

    feat: usb, device定義

--- day06/kernel-06/src/usb/xhci/device.rs
+++ day06/kernel-06/src/usb/xhci/device.rs
@@ -1,9 +1,22 @@
 // ================================================================
 // @file usb/xhci/device.rs
 //
-// USB デバイスを表すクラスと関連機能．
+// xHCI固有のデバイスを表すクラスと関連機能．
 // ================================================================
 
-pub struct Device{
 
+enum State{
+
+}
+pub struct Device{
+    slot_id: u8,
+
+}
+
+impl Device{
+    pub fn new(slot_id: u8) -> Self{
+        Self{
+            slot_id,
+        }
+    }
 }
