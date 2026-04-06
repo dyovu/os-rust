3640ef21a82f40c729b800321753d5b9969f654c
dyovu <131934396+dyovu@users.noreply.github.com>
2026-04-06 19:23:53 +0900

    feat: alloc_mem実装

--- day06/kernel-06/src/usb/memory_alloc.rs
+++ day06/kernel-06/src/usb/memory_alloc.rs
@@ -10,17 +10,14 @@ use core::mem::size_of;
 // 動的メモリ確保のためのメモリプールの最大容量（バイト）
 const MEMORY_POOL_SIZE:usize = 4096 * 32;
 
-
-fn ceil<T>(ptr: *const T , alignment: usize) -> *const T{
+fn ceil(ptr: usize, alignment: usize) -> usize{
     let addr = ptr as usize;
-    let aligned_addr = (addr + alignment - 1) & !((alignment - 1) as usize);
-    aligned_addr as *const T
+    (addr + alignment - 1) & !((alignment - 1) as usize)
 }
 
-fn mask_bits<T>(ptr: *const T , mask: usize) -> *const T{
+fn mask_bits(ptr: usize , mask: usize) -> usize{
     let addr = ptr as usize;
-    let masked_addr = addr & !(mask -1);
-    masked_addr as *const T
+    addr & !(mask -1)
 }
 
 struct MemoryPool {
@@ -29,11 +26,28 @@ struct MemoryPool {
 }
 
 impl MemoryPool{
-    fn alloc_mem(&self, size: usize, alignment: usize, boundary: usize) -> usize {
-        0
+    fn alloc_mem(&mut self, size: usize, alignment: usize, boundary: usize) -> usize {
+        if alignment > 0{
+            self.alloc_ptr = ceil(self.alloc_ptr, alignment);
+        }
+        if boundary > 0 {
+            let next_boundary = ceil(self.alloc_ptr, boundary);
+            if next_boundary < self.alloc_ptr + size {
+                self.alloc_ptr = next_boundary;
+            }
+        }
+
+        let pool_start = self.pool.as_ptr() as usize;
+        if pool_start + MEMORY_POOL_SIZE < self.alloc_ptr + size {
+            return 0;
+        }
+
+        let p = self.alloc_ptr;
+        self.alloc_ptr += size;
+        p
     }
 
-    pub fn alloc_array<T>(&self, num_obj: usize, alignment: usize, boundary: usize) -> *const T {
+    pub fn alloc_array<T>(&mut self, num_obj: usize, alignment: usize, boundary: usize) -> *const T {
         self.alloc_mem(size_of::<T>()*num_obj, alignment, boundary) as *const T
     }
 
