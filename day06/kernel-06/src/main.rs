393069ae3f9172b2022e3c5b119a39829bdd61ad
dyovu <131934396+dyovu@users.noreply.github.com>
2026-04-10 17:25:32 +0900

    update: コメントアウト

--- day06/kernel-06/src/main.rs
+++ day06/kernel-06/src/main.rs
@@ -284,7 +284,7 @@ pub extern "sysv64" fn _start(
     };
 
     let xhc_controller = Controller::new(xhc_mmio_base);
-    printk!("max_ports: {}", xhc_controller.max_ports);
+    // printk!("max_ports: {}", xhc_controller.max_ports);
 
     if 0x8086 == pci::read_vendor_id_from_dev(&xhc_device){
         pci::switch_ehci2xhci(&xhc_device);
